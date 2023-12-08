0fbbd50a9b94bdc75bc38a8969f3d1ed03d38248
Abhishek-EE <abhishekranjansingh014@gmail.com>
2023-12-08 11:57:46 -0600

    Squashed commit of the following:
    commit 83f109b24086ed5756bba3e12d2d675d2c80a88d
    Author: Abhishek-EE <abhishekranjansingh014@gmail.com>
    Date:   Fri Dec 8 11:56:48 2023 -0600
    
        Added the merge capability
    
    commit ecfd194e52df84204dbc12189a60c08a62a10fce
    Author: Abhishek-EE <abhishekranjansingh014@gmail.com>
    Date:   Fri Dec 8 11:56:06 2023 -0600
    
        Added generateGridFromImage Functionality

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,26 @@
 int main() {
 
   // std::unique_ptr<PathPlannerInterface> planner;
-  Eigen::Vector3f start(0.0f, 0.0f, 0.0f); // Example start position
+  Eigen::Vector3f start(2.1f, 3.2f, 0.0f); // Example start position
   Eigen::Vector3f end(15.0f, 15.0f, 0.0f); // Example end position
   const float robotDia = 0.6;
   bool isValid= true;
-  float occupancyratio = 0.5;
+  float occupancyratio = 0.2;
 #ifdef INTERFACE_NOT_IMPLEMENTED
   std::cout << "Hello World!\n";
 #else
-  // grid = std::make_unique<PathPlannerImplementation>();/// PathPlannerImplementation is the derived class
-  // auto start = Eigen::Vector3f::Identity();
-  // auto end = Eigen::Vector3f::Identity();
-  // planner->getCollisionFreePath(grid, start, end);
+  Trajectory validPath;
   GenerateGridImplementation gridGenerator(robotDia,occupancyratio);
   gridGenerator.generateGrid(start,end,isValid);
   const OccupancyGrid& grid = gridGenerator.getGrid();
-  // grid.visualizeGrid(start,end);
-  // grid.visualizeGrid(start,end);
   visualizeGrid(grid,start,end);
+  validPath = gridGenerator.getValidTrajectory();
+  visualizeGridAndTrajectory(grid,validPath);
+  OccupancyGrid grid2;
+  generateOccupancyGridFromImage(grid2,"/home/abhishek/dev/Planning_Assingment/grid-example.png");
+  visualizeGrid(grid2,start,end);
+
+
 
 
 #endif
